b148170cd2679d0889dce05ecbf9208b7d1ea58f
Manuela Bognar <manuelabcamara@gmail.com>
2020-09-04 16:04:50 -0300

    relationship models

--- backend/src/database/migrations/1598984761801-CreateUsers.ts
+++ backend/src/database/migrations/1598984761801-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1598984761801 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
