b1a14eafa3efecb034ecdcf350520e3f6a365a4a
Manuela Bognar <manuelabcamara@gmail.com>
2020-09-04 18:59:14 -0300

    creating token JWT

--- backend/src/services/AuthenticationUserService.ts
+++ backend/src/services/AuthenticationUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -8,11 +9,13 @@ interface Request {
     password: string;
 }
 
+interface Response {
+    user: User;
+    token: string;
+}
+
 class AuthenticateUserService {
-    public async execute({
-        email,
-        password,
-    }: Request): Promise<{ user: User }> {
+    public async execute({ email, password }: Request): Promise<Response> {
         const usersRepository = getRepository(User);
 
         const user = await usersRepository.findOne({ where: { email } });
@@ -30,8 +33,14 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination');
         }
 
+        const token = sign({}, 'cecddbe7f3229f908706602ea1d17db0', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
         return {
             user,
+            token,
         };
     }
 }
