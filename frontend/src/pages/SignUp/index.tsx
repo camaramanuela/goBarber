52bb15c44709b159f46b40917c4842276b9e8494
Manuela Bognar <manuelabcamara@gmail.com>
2020-11-07 15:39:43 -0300

    animations

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -11,7 +11,7 @@ import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
@@ -40,23 +40,25 @@ const SignUp: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="logo GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="logo GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit} >
-          <h1>Faça seu cadastro</h1>
+          <Form ref={formRef} onSubmit={handleSubmit} >
+            <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            
+            <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
           
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-        
-          <Button type="submit">Entrar</Button>
-        </Form>
+            <Button type="submit">Entrar</Button>
+          </Form>
 
-        <Link to="/">
-          <FiArrowLeft />
-          Voltar para o login
-        </Link>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para o login
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
